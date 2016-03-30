4a7620f247789d8cc6a03f5ecf3b7cbe42ca27a5
itsajamesday <Toxic.Raptor24@gmail.com>
2016-03-30 20:20:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ wget https://www.privateinternetaccess.com/installer/port_forward.sh
 # mv $HOME/Downloads/apacheport.sh $HOME/apacheport.sh
 chmod +x port_forward.sh
 chmod +x apacheport.sh
-deb "alias externalip="wget http://ipinfo.io/ip -qO -"" >> .bashrc
-deb "alias port="$HOME/port_forward.sh p2768382 jiuo9y96"" >> .bashrc
-deb "alias cleanapache="rm /var/www/*"" >> .bashrc
+echo "alias externalip="wget http://ipinfo.io/ip -qO -"" >> .bashrc
+echo "alias port="$HOME/port_forward.sh p2768382 jiuo9y96"" >> .bashrc
+echo "alias cleanapache="rm /var/www/*"" >> .bashrc
 
